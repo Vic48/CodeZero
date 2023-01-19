664e312a0ff69a4f1c15a8bd478e491e8e26edf3
lianqi541 <holianqi@gmail.com>
2023-01-19 12:42:46 +0800

    Updates
    - Main Menu (done)
    - Pause Menu (WIP)
    - Level Select (WIP)

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelControl : MonoBehaviour
 {
+    public Button[] levelButtons;
+    public int sceneID;
+
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 2 > levelAt)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
+
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
